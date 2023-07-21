0330883dab1bf6b1cd8910cd982f758c51657341
Ayomide Oluwadiya <victordiya90@gmail.com>
2023-07-21 21:05:42 +0100

    set up router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,45 @@
-import React from "react";
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import Outlet from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+const Container = styled.div``;
+
+const Main = styled.div``;
 
 const App = () => {
-  return <div>App</div>;
+  const Layout = () => {
+    return (
+      <>
+        <Menu />
+        <Main>
+          <Navbar />
+          <Outlet />
+        </Main>
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <Container>
+      <RouterProvider router={router} />
+    </Container>
+  );
 };
 
 export default App;
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Menu = () => {
+  return <div>Menu</div>;
+};
+
+export default Menu;
