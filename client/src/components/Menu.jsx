d860142889d06e1f8a920bb00655007973321bf8
Ayomide Oluwadiya <victordiya90@gmail.com>
2023-07-25 11:04:52 +0100

    designed nav bar

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -18,7 +18,7 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 
 const Container = styled.div`
   flex: 1;
-  background-color: ${({ theme }) => theme.bg};
+  background-color: ${({ theme }) => theme.bgLighter};
   height: calc(100vh - 50px);
   color: ${({ theme }) => theme.text};
   font-size: 14px;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,67 @@
 import styled from "styled-components";
 import logo from "../img/logo.png";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 
 const Container = styled.div`
   height: 50px;
   position: sticky;
   top: 0;
-  background-color: #202020;
-  color: white;
+  background-color: ${({ theme }) => theme.bgLighter};
+  color: ${({ theme }) => theme.text};
+  border-bottom: 2px solid black;
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const Logo = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
   font-weight: bold;
-  padding: 18px 26px;
+  margin-left: 26px;
 `;
 
 const Img = styled.img`
   height: 25px;
+`;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  width: 40%;
+  justify-content: space-between;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  outline: 0;
+  border: none;
+  background-color: transparent;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 5px 10px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500px;
+  cursor: pointer;
+  margin-right: 26px;
 `;
 
 const Navbar = () => {
@@ -31,6 +72,14 @@ const Navbar = () => {
           <Img src={logo} />
           MideTube
         </Logo>
+        <Search>
+          <Input type="text" placeholder="Search" />
+          <SearchOutlinedIcon />
+        </Search>
+        <Button>
+          <AccountCircleOutlinedIcon />
+          SIGN IN
+        </Button>
       </Wrapper>
     </Container>
   );
